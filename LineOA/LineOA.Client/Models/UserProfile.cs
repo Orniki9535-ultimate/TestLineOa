6890098f87d5c16bcba15c9679ccaccdd93324ba
Sorayut Butbao <sorayut.b@eofficebot.com>
2026-02-04 16:02:40 +0700

    Initial LIFF Project

---
+++ LineOA/LineOA.Client/Models/UserProfile.cs
@@ -0,0 +1,7 @@
+﻿public class UserProfile
+{
+    public string UserId { get; set; }
+    public string DisplayName { get; set; }
+    public string PictureUrl { get; set; }
+    public string StatusMessage { get; set; }
+}
---
+++ LineOA/LineOA/wwwroot/liff-starter.js
@@ -0,0 +1,22 @@
+﻿window.liffFunctions = {
+    init: async (liffId) => {
+        try {
+            await liff.init({ liffId: liffId });
+            if (liff.isLoggedIn()) {
+                return true;
+            } else {
+                liff.login();
+                return false;
+            }
+        } catch (error) {
+            console.error("LIFF Init Error:", error);
+            return false;
+        }
+    },
+    getProfile: async () => {
+        if (liff.isLoggedIn()) {
+            return await liff.getProfile();
+        }
+        return null;
+    }
+};
